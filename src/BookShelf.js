e4fd153c4abfcce8838bcf1f767c8eb6e4829cda
alkami-io <robert@alkami.io>
2017-12-05 10:48:47 -0700

    feat: Added ShelfChanger Component Extracted ShelfChanger code from BookShelf.js and made it it's own stateless functional component. This component will likely have to be turned into a controlled component in order to track its state based on the shelf that the book is on. The Changers state will need to change based on its shelf. Or we can look at passing it's state as a prop from the book book which will be tracking it's state already.  No need for two components to track the same state if the state can be passed as a prop from the book to the changer... Research more as we continue to code.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import ShelfChanger from './ShelfChanger'
 
-// Books Arrays for Initial Build Out Not needed once We are pulling data from the API
 const BookShelf = (props) => (
     <div>
         <div className='bookshelf'>
+            {/*Get ShelfName from props when component is mounted*/}
             <h2 className='bookshelf-title'>{props.shelfName}</h2>
             <div className='bookshelf-books'>
                 <ol className="books-grid">
+
+                    {/*Get List of books from props iterate and list*/}
                     {props.bookList.map(book =>
                         <li key={book.title}>
                             <div className="book">
@@ -18,15 +21,9 @@ const BookShelf = (props) => (
                                              backgroundImage: `url("${book.coverImage}")`
                                          }}>
                                     </div>
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="none" disabled>Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
+
+                                    {/*Shelf Changer Component for each book*/}
+                                    <ShelfChanger/>
                                 </div>
                                 <div className="book-title">{book.title}</div>
                                 <div className="book-authors">{book.author}</div>
